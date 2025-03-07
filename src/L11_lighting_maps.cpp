774a076e017a20b90dd510e598c53a8eb8c600bc
bestmafen <bestmafen@163.com>
2025-03-07 22:26:49 +0800

    优化光照贴图

--- src/L11_lighting_maps.cpp
+++ src/L11_lighting_maps.cpp
@@ -175,8 +175,28 @@ int L11_lighting_maps_main()
         std::cout << "Failed to load texture" << std::endl;
     }
 
+    GLuint texture1;
+    glGenTextures(1, &texture1);
+    glBindTexture(GL_TEXTURE_2D, texture1);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+   data = stbi_load(IMAGE_DIR"img3.png", &width, &height, &nrChannels, 0);
+//     std::cout << "width=" << width << ", height=" << height << ", nrChannels=" << nrChannels << std::endl;
+    if (data)
+    {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+        stbi_image_free(data);
+    } else
+    {
+        std::cout << "Failed to load texture" << std::endl;
+    }
+    
     objectShader.use();
     objectShader.setInt("material.diffuse", 0);
+    objectShader.setInt("material.specular", 1);
 
     // render loop
     // -----------
@@ -219,6 +239,8 @@ int L11_lighting_maps_main()
         glBindVertexArray(VAO);
         glActiveTexture(GL_TEXTURE0);
         glBindTexture(GL_TEXTURE_2D, texture);
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, texture1);
         // calculate the model matrix for each object and pass it to shader before drawing
         glm::mat4 model; // make sure to initialize matrix to identity matrix first
         model = glm::rotate(model, (float) glfwGetTime() * glm::radians(20.f), glm::vec3(.5f, 1.f, .2f));
